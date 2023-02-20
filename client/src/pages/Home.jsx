193a47c3697900ebfb2eb5e6946be58d86f9b8c5
Aaron Chan <a20chan@ryerson.ca>
2023-02-20 17:35:01 -0500

    build

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,12 +27,15 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:3000/api/v1/post', {
-          method: "GET",
-          headers: {
-            'Content-Type' : 'application/json',
-          },
-        })
+        const response = await fetch(
+          "https://dall-e-clone-9oim.onrender.com/api/v1/post",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
         if (response.ok) {
           const result = await response.json();
